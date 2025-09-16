1431782f57dc92b4af270a7a10ba734da057e7a6
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-09-16 17:37:07 +0600

    Python

---
+++ what_will_be_the_output_01.py
@@ -0,0 +1,9 @@
+"""Let a string be "Hello,World!" 
+
+what will be the output of s[ :7], s[7:], s[1:5], s[-5:-2] ?"""
+
+s = "Hello,World!"
+print(s[:7])    # Output: Hello,W
+print(s[7:])    # Output: orld!
+print(s[1:5])   # Output: ello
+print(s[-5:-2]) # Output: orl
