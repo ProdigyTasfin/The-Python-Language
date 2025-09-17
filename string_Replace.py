a3989d4fbcca3a2fd632d9457ca3a8cc5c92b122
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-09-17 21:26:18 +0600

    Python

--- string_Replace.py
+++ string_Replace.py
@@ -7,3 +7,8 @@ Dear <|NAME|>,
 '''
 
 print(letter.replace("<|NAME|>", "Tasfin").replace("<|POSITION|>", "Software Engineer").replace("<|DATE|>", "1st Dec 2025"))
+
+#Explaination: The replace() method in Python is used to replace occurrences of a specified substring with another substring. 
+# In this code, we have a multi-line string letter that contains placeholders for a name, position, and date. 
+# We use the replace() method three times in a chained manner to replace each placeholder with the corresponding actual value. \
+# The final output is a personalized letter with the placeholders replaced by "Tasfin", "Software Engineer", and "1st Dec 2025" respectively.
