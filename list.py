3deeeb2b7c15069a116161ca169abb329b82d688
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-01 21:56:59 +0600

    python

--- list.py
+++ list.py
@@ -6,33 +6,34 @@ friends = ["Alice", "Apple", True, 10, 3.14, None, "Charlie", "Bob"]
 
 print(friends[1])
 friends[1] = "Mike"
-print(friends[1])
+print("index 1:", friends[1])
 
 # print list index 0 to 4
-print(friends[0:4]) 
+print("friends[0:4]:", friends[0:4])
 # see the output! ['Alice', 'Mike', True, 10] there Apple --> Mike
 
 friends.append("Sakib")
-print(friends)
+print("Adding new friends in the end: ", friends)
 # ['Alice', 'Mike', True, 10, 3.14, None, 'Charlie', 'Bob', 'Sakib'] 
 
 friends.remove(True)
-print(friends)
+print("Removed True from friends: ", friends)
 # ['Alice', 'Mike', 10, 3.14, None, 'Charlie', 'Bob', 'Sakib']
 
-friends.pop()
-print(friends)
+friends.pop(2)
+print("pop-out friends: ", friends)
 # ['Alice', 'Mike', 10, 3.14, None, 'Charlie', 'Bob']
-# removes the last element
+# removes the element at index 2
+# output: ['Alice', 'Mike', 3.14, None, 'Charlie', 'Bob']
 
-friends.sort()  # will sort only the string elements and give error if other data types are present
-print(friends) 
-# ['Alice', 'Bob', 'Charlie', 'Mike', 10, 3.14, None]
+friends.sort(key=str)  # will sort only the string elements and give error if other data types are present
+print("Sorted friends: ", friends) 
+# output: ['Alice', 'Bob', 'Charlie', 'Mike', 10, 3.14, None]
 # Suppose a = [5,2,3, 1,4,6]
 # output: [1, 2, 3, 4, 5, 6]
 
 friends.reverse()
-print(friends)
+print("Reversed friends: ", friends)
 # [None, 3.14, 10, 'Mike', 'Charlie', 'Bob', 'Alice']
 # reverses the list
 
@@ -40,3 +41,7 @@ print(friends.index("Bob"))
 # 5
 print(friends)
 # ouuput: [None, 3.14, 10, 'Mike', 'Charlie', 'Bob', 'Alice']
+
+friends.insert(2, "Tasfin")
+print(friends)
+# [None, 3.14, 'Tasfin', 10, 'Mike', 'Charlie', 'Bob', 'Alice']
