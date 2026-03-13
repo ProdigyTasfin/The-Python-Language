8ff3e40f8e1163c3fb7b07bd269a3f946bfcb365
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-03-13 16:27:36 +0600

    python

--- variables.py
+++ variables.py
@@ -1,5 +1,14 @@
 a = 1
-
 b = 2
 
 print(a+b)
+
+name = "Tasfin"
+name1 = 'Sakib'
+sentence = "This is a sentence!"
+sentence1 = 'This is an another sentence!'
+age = 23
+weight = 65.45
+
+print(sentence1)
+print("His name:",name,"and his age is",age,"years.","Weight:", weight)
