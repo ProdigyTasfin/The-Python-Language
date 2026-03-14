480d7d31ff4d8b82841edc3c44de8f8a172ecf3f
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-03-14 17:43:29 +0600

    String

--- string_Slice.py
+++ string_Slice.py
@@ -3,5 +3,8 @@
 s = input("Enter a string: ")
 
 q = s[0:3]
+p = s[-4:-1]
 
 print("The slice of the string from index 0 to 3 is:", q)
+
+print("The slice of the string from index -4 to -1 is:", p) 
