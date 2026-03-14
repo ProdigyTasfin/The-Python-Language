47c5626b66114fe4e2d9b568a779a884887bdbb3
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-03-14 21:57:07 +0600

    Python

---
+++ Practice05.py
@@ -0,0 +1,6 @@
+# Q: Write a Program to input user's first name and print its length
+# Difficulty: Easy
+
+str = input("Enter Your First Name: ")
+
+print(len(str))
---
+++ Practice06.py
@@ -0,0 +1,8 @@
+# Q: Write to find the occurrence of '$' in a String
+
+# Difficulty: Intermediate
+
+str = input("Enter Your String: ")
+
+occurrence = str.count("$")
+print("In your String, '$' occurs: ", occurrence)
