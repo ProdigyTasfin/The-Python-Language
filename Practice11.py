ee21d5f954064a59ef1f1dd4d346805abab42ae7
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-03-15 02:33:08 +0600

    Python

---
+++ Practice11.py
@@ -0,0 +1,18 @@
+# List:
+
+# [3,6,9,12,15]
+
+# Use a loop to print:
+# 6 12 18 24 30 
+
+#Difficulty: Intermediate
+
+numbers = [3,6,9,12,15] 
+
+print("PREVIOUS LIST: ",numbers)
+
+
+print("NEW LIST: ")
+
+for n in numbers:
+    print(n * 2)
---
+++ Practice12.py
@@ -0,0 +1,13 @@
+""" Loop through the word
+
+"Python"
+
+and print each letter. """ 
+
+# Difficulty: Intermediate
+
+str = "Python" 
+
+for letters in str:
+    print(letters)
+
