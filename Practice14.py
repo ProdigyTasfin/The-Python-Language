260fade5ac27ec5cff336d9cea9424e0671c8906
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-03-15 17:47:50 +0600

    Practice

---
+++ Practice14.py
@@ -0,0 +1,10 @@
+# Program to check if a number is a multiple of 7 or not. 
+
+# Difficulty: Easy
+
+num = int(input("Number: "))
+
+if (num % 7 == 0):
+    print(num, "is multiple of 7!")
+else:
+    print(num, "is NOT multiple of 7!")
